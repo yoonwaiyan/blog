be93fac9954d47930094c7f71aed1114bc407ad2
Yoon Wai Yan <waiyanyoon@gmail.com>
2018-03-05 22:03:43 +0800

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,16 @@ module.exports = {
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-115013002-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+      }
     }
   ]
 };
